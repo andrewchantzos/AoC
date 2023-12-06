5623082939fe9af6e134541e54ee61799bad5a11
achantzos <achantzos@bloomberg.net>
2023-12-06 15:42:51 +0000

    Day 6, update

--- src/aoc2023/day06.py
+++ src/aoc2023/day06.py
@@ -1,8 +1,6 @@
 from aocd.models import Puzzle
 import re
 from src.utils import *
-from functools import reduce
-from collections import defaultdict
 
 puzzle = Puzzle(year=2023, day=6)
 
@@ -14,14 +12,14 @@ dist_ints = [int(match) for match in re.findall(r"\b\d+\b", inp[1])]
 
 pairs = zip(time_ints, dist_ints)
 
-winning = defaultdict(int)
+res = 1
 for t, d in pairs:
-    combos = []
     for i in range(t):
-        winning[d] += int((i * (t - i)) > d)
+        if i * (t - i) > d:
+            res *= len(range(i, t - i + 1))
+            break
 
-
-puzzle.answer_a = reduce(lambda x, y: x * y, winning.values())
+puzzle.answer_a = res
 
 
 # part two
